d22ef5888405d2214eff1f8f813bd9811f13871d
Sukhman75 <sukhmansingh75@gmail.com>
2018-11-12 03:05:01 -0500

    Game Logic added with Start_again Functionality

--- Quote_Scraping.py
+++ Quote_Scraping.py
@@ -28,29 +28,41 @@ while url2:
  #sleep(2)	
  #uncomment sleep(2) to slow down scraping
 #Adding game logic
-
-quote = choice(all_quotes)
-print(f"Here's the Quote:\n{quote['text']} ")
-print(quote["authors"])
-guess = ''
-Remaining_Guesses = 4
-while guess.lower() != quote["authors"].lower() and Remaining_Guesses > 0:
- guess = input(f"Who is author of this QUOTE ? \n Guesses Remaining: {Remaining_Guesses}\n")
- Remaining_Guesses -= 1
- if Remaining_Guesses == 3:
-  res = requests.get(f"{url1}{quote['links']}")
-  soup = BeautifulSoup(res.text, "html.parser")
-  Date_of_birth = soup.find(class_="author-born-date").get_text()
-  Place_of_birth = soup.find(class_ = "author-born-location").get_text()
-  print(f"Hint No. 1, author was born in {Date_of_birth} {Place_of_birth}.")
- elif Remaining_Guesses == 2:
-  First_initial = quote["authors"][0]
-  print(f"Hint No. 2, author's first name initial is: {First_initial}")
- elif Remaining_Guesses == 1:
-    Last_initial = quote["authors"].split(" ")
-    print(f"Hint No. 3, author's last name initial is: {Last_initial[1][0]}")   
- else:
-    print(f"Sorry all the guesses were wrong!!!\nAnswer is: {quote['authors']}")   	
-
+def start_game():
+	quote = choice(all_quotes)
+	print(f"\n>>>Here's the Quote:\n{quote['text']} ")
+	print(quote["authors"])
+	guess = ''
+	Remaining_Guesses = 4
+	while guess.lower() != quote["authors"].lower() and Remaining_Guesses > 0:
+	 guess = input(f"Who is author of this QUOTE ? \n Guesses Remaining: {Remaining_Guesses}\n")
+	 Remaining_Guesses -= 1
+	 if guess.lower() == quote["authors"]:
+	 	print("Your guess is right!!")
+	 	break
+	 if Remaining_Guesses == 3:
+	  res = requests.get(f"{url1}{quote['links']}")
+	  soup = BeautifulSoup(res.text, "html.parser")
+	  Date_of_birth = soup.find(class_="author-born-date").get_text()
+	  Place_of_birth = soup.find(class_ = "author-born-location").get_text()
+	  print(f"Hint No. 1, author was born in {Date_of_birth} {Place_of_birth}.")
+	 elif Remaining_Guesses == 2:
+	  First_initial = quote["authors"][0]
+	  print(f"Hint No. 2, author's first name initial is: {First_initial}")
+	 elif Remaining_Guesses == 1:
+	    Last_initial = quote["authors"].split(" ")
+	    print(f"Hint No. 3, author's last name initial is: {Last_initial[1][0]}")   
+	 elif Remaining_Guesses <=0 :
+	    print(f"Sorry all the guesses were wrong!!!\nAnswer is: {quote['authors']}")   	
+	Play_again = ''
+	while Play_again.lower() not in ('y', 'yes', 'n', 'no'):
+	 Play_again= input("Do you want to play again ?{y/n}")
+	 if Play_again.lower() in ('y', 'yes'):
+	 	return start_game()
+	 else:
+	    print("GOODBYE!!")	
+ 
+start_game()
+ 
 
 print("out of loop")	
